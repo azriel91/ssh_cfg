ffef7505ef0e5914f92ccb5bca2a19d2cdf7738a
Azriel Hoh <azriel91@gmail.com>
2022-12-30 17:04:08 +1300

    Address clippy lints.

--- src/ssh_section.rs
+++ src/ssh_section.rs
@@ -14,9 +14,9 @@ pub enum SshSection {
 impl fmt::Display for SshSection {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::Host(host) => write!(f, "Host {}", host),
-            Self::Match(r#match) => write!(f, "Match {}", r#match),
-            Self::Include(include) => write!(f, "Include {}", include),
+            Self::Host(host) => write!(f, "Host {host}"),
+            Self::Match(r#match) => write!(f, "Match {match}"),
+            Self::Include(include) => write!(f, "Include {include}"),
         }
     }
 }
